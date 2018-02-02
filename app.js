8ae37fd75b1219ad6f43ae67cd3e237de2c7239c
Dyego Eugenio <deugenio@daitangroup.com>
2018-02-02 11:29:40 -0200

    add api path

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var os = require('os');
 
+app.get('/health', function (req, res) {
+  res.send('OK');
+});
+
 app.get('/', function (req, res) {
-  res.send('Node API: ' + os.hostname());
+  res.send('[API]: ' + os.hostname());
 });
 
 app.listen(3000, function () {
